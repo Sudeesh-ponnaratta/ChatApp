c15c8bb53459daa771dd51ac530360777ebac4ad
Sudeesh <sudeeshponnaratta@gmail.com>
2023-11-24 18:53:48 +0400

    Authentication and Web tokens

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-	public class UserController : ControllerBase
+namespace API.Controllers
+{
+	[Authorize]
+	public class UserController : BaseApiController
 	{
 		private readonly DataContext _context;
 
@@ -18,18 +18,19 @@ namespace API.Controllers;
 		{
 			_context = context;
 		}
-		
+		[AllowAnonymous]
 		[HttpGet]
 		public async Task<ActionResult<IEnumerable<AppUser>>> getUser()
 		{
-		    var users = await  _context.Users.ToListAsync();
-		    return users;
+			var users = await  _context.Users.ToListAsync();
+			return users;
 		}
 		[HttpGet("{id}")]
 		public async Task<ActionResult<AppUser>>getUser(int id)
 		{
-		    return await _context.Users.FindAsync(id);
+			return await _context.Users.FindAsync(id);
 		
 		}
 	
+}
 }
